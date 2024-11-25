79a800f755dee6d05a86d96a01e98fc7f1d703b8
prashantshrotriyas <prashantshrotriyas@gmail.com>
2024-11-25 18:36:34 +0530

    The bash script file to create the directory and file structures

---
+++ Initialize.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+mkdir -p "Assignment4"
+
+cd "Assignment4"
+
+touch Faculty_Affiliations.csv
+touch publication.h
+touch publication.cpp
+touch bibparser.h
+touch bibparser.cpp
+touch question2.cpp
+touch question3.cpp
+touch Makefile_P2
+touch Makefile_P3
+touch README.md
+touch faculty.csv
+
+echo "All files created directly in the 'Assignment 4' directory."
